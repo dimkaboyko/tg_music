0545dcff06029d4bb81d549cb90b24615de4e75f
Dima Boyko <dimka.boyko@hotmail.com>
2015-12-09 21:56:02 +0100

    fixed vk

--- tg_music.rb
+++ tg_music.rb
@@ -3,17 +3,15 @@ require 'net/http'
 require 'json'
 require 'open-uri'
 require 'uri'
+require "vk-ruby"
 token = '125138084:AAGSYcxwt_yJTyrhLTyqgviCmvwb1-2A-C4'
+
+
+
 def download_song(song_name)
-  url = "https://api.vk.com/method/audio.search?
-  access_token=7f8cbb7c49ef587fe95e041eb2a28325d79dce374
-  7c97ce708fa5fbeed2990c78ef30eecf9d692520a6fe&q
-  ="+song_name+"&count=1"
-  url = URI.parse(URI.encode(url)).to_s.gsub("%0A%20%20","")
-  uri = URI(url)
-  response = Net::HTTP.get(uri)
-  p uri
-  song_url = JSON.parse(response)["response"][1]["url"]
+  app = VK::Application.new(app_id: 4930402, version: '5.37', access_token:'7f8cbb7c49ef587fe95e041eb2a28325d79dce3747c97ce708fa5fbeed2990c78ef30eecf9d692520a6fe')
+  song =  app.audio.search q: song_name, count: 1
+  song_url =  song["items"][0]["url"]
   open('song_from_vk.ogg', 'wb') do |file|
     file << open(song_url).read
   end
